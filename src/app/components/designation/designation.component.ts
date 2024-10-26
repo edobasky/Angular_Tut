55b76c72ca61f6ac108baa8c9c570be4932b6799
edobasky <edore7@gmail.com>
2024-10-26 19:26:35 +0100

    routing,new component

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -12,14 +12,16 @@ import { APIResponseModel, IDesignation } from '../../model/interface/role';
 export class DesignationComponent implements OnInit {
 
   masterService = inject(MasterService)
-
+  isLoader : boolean = true;
   designationList : IDesignation[] = [];
 
   ngOnInit(): void {
     this.masterService.getDesignations().subscribe((result:APIResponseModel) => {
     this.designationList = result.data;
+    this.isLoader = false;
     },error => {
       alert("API error/ Network Down")
+      this.isLoader = false
     })
   }
 
